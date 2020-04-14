65e0ec0fbaa1da336581cd89cea9fd98ecf66cd5
Juan Manuel Cañabate <juanmanuel@patagonian.it>
2020-04-14 07:16:11 -0300

    add fps meter

--- js/run.js
+++ js/run.js
@@ -3,6 +3,51 @@ const GRID_COLOR = "#EEEEEE";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 
+class FPS {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = (1 / delta) * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+}
+
+const fps = new FPS();
+
 export const run = async ({ Universe, Cell }, memory) => {
   const universe = Universe.new();
   const width = universe.width();
@@ -125,6 +170,7 @@ export const run = async ({ Universe, Cell }, memory) => {
   });
 
   const renderLoop = () => {
+    fps.render();
     drawGrid();
     drawCells();
     universe.tick();
